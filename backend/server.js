69c1b4c37b4728c3fb2d1cbc44645b1ccdabdf77
Ahmed Waseem <ah.waseem2005@gmail.com>
2024-12-07 11:27:17 -0500

    prepared the app for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import notificationRoutes from "./routes/notification.route.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
@@ -18,6 +19,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json({limit:"5mb"}));
 app.use(express.urlencoded({ extended: true }))
@@ -27,6 +29,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
